2d8ce3f9293040ad935d9b048855bbb79d492973
F5PC <F5pc.com@gmail.com>
2026-05-10 21:52:58 -0700

    Optimize images with WebP

---
+++ optimize-images.cjs
@@ -0,0 +1,32 @@
+const sharp = require("sharp");
+
+const images = [
+  {
+    input: "./public/images/hero-bg.png",
+    output: "./public/images/hero-bg.webp",
+    width: 1920,
+  },
+  {
+    input: "./public/images/sitebg.png",
+    output: "./public/images/sitebg.webp",
+    width: 1920,
+  },
+  {
+    input: "./public/preview.jpg",
+    output: "./public/preview.webp",
+    width: 1400,
+  },
+];
+
+async function optimize() {
+  for (const image of images) {
+    await sharp(image.input)
+      .resize({ width: image.width })
+      .webp({ quality: 82 })
+      .toFile(image.output);
+
+    console.log(`Optimized: ${image.output}`);
+  }
+}
+
+optimize();
